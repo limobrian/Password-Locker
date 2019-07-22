2952862382ebb42de2c27446dd42777a0495a9ba
briankiplimo <limobrian290@gmail.com>
2019-07-22 10:50:51 +0300

    enerating passwords

--- credential.py
+++ credential.py
@@ -37,3 +37,12 @@ def generate_password(cls):
         password = ''.join( choice(alphanum) for num in range(size) )
         
         return password
+
+         # user_credential_list = []
+
+        # for credential in cls.credential_list:
+        #     if credential.user_password == password:
+        #         user_credential_list.append(credential)
+
+        # return user_credential_list
+    
